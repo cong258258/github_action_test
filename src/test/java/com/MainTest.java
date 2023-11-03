6a0462e8a82a297baf4bdd537feb73d3b014d48a
cong258258 <1615223768@qq.com>
2023-11-03 14:52:22 +0800

    more test cases

--- src/test/java/com/MainTest.java
+++ src/test/java/com/MainTest.java
@@ -11,8 +11,8 @@ class MainTest {
 		Main main = new Main();
 		int result = main.add(1, 1);
 		assertEquals(2, result);
-		int res2 = main.add(-Integer.MAX_VALUE, 5);
-		assertEquals(-2147483642, res2);
+		int res2 = main.add(1234, 5);
+		assertEquals(1239, res2);
 
 	}
 
